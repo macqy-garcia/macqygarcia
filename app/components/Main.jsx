a2f2ab776f24b921ed89ca050fed6e8156d49120
Macqy Garcia <garciamacqy@gmail.com>
2023-04-26 02:56:12 +0800

    ignore

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -199,7 +199,6 @@ export const Main = ({ isActive, setisActive }) => {
           is always open. Whether you have a question or just want to say hi,
           I’ll try my best to get back to you!
         </p>
-
         <button className="relative left-0 top-0 z-10 rounded border border-[#64ffda] bg-[#0D192D] px-5 py-4 font-mono text-sm text-[#64ffda] transition duration-300 ease-in-out hover:-translate-x-[1px] hover:-translate-y-[1px] hover:border-b-4 hover:border-r-4">
           <a href="mailto:garciamacqy@gmail.com">Say Hello!</a>
         </button>
